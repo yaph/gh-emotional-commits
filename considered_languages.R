891e3ae33caf2cde9f8b8e6969e44055947dd13f
Ramiro Gomez <web@ramiro.org>
2012-05-09 01:24:30 +0200

    added considered languages script

---
+++ considered_languages.R
@@ -0,0 +1,22 @@
+# comment.char = "" must be set due to # char in C# 
+tcir = read.table("considered_languages.csv", sep=",", header=TRUE, comment.char = "")
+sorted_by_ratio <- tcir[with(tcir, order(-cntlang, repository_language)), ]
+
+png(filename="considered-languages-commit-count.png",
+    width = 800, height = 600, units = "px", pointsize = 14
+)
+
+# suppress scientific notation of numbers
+options(scipen=3)
+
+# vertical labels: las=2, margins: mar=c(bottom, left, top, right)
+par(las=2, mar=c(6, 6, 2, 2),mgp=c(4.5,1,0))
+bp <- barplot(sorted_by_ratio$cntlang,
+    ylab="Commit Message Count",
+    names.arg=sorted_by_ratio$repository_language,
+    col=heat.colors(nrow(tcir)),
+    log(sorted_by_ratio$cntlang)
+)
+
+# close device
+dev.off()
--- issues.R
+++ issues.R
@@ -4,7 +4,7 @@
 # comment.char = "" must be set due to # char in C# 
 tcir = read.table("merged_count_issues.csv", sep=",", header=TRUE, comment.char = "")
 # order by issue_count descending then by language name
-sorted_by_ratio <- tcir[with(tcir, order(-percentage, language)), ]
+sorted_by_ratio <- tcir[with(tcir, order(-percentage, repository_language)), ]
 
 # open a device using png() and specify taget image file
 png(filename="commit-messages-issues-percentage.png",
@@ -12,12 +12,12 @@ png(filename="commit-messages-issues-percentage.png",
 )
 
 # vertical labels: las=2, margins: mar=c(bottom, left, top, right)
-par(las=2, mar=c(7, 6, 4, 2))
+par(las=2, mar=c(6, 6, 4, 2))
 bp <- barplot(sorted_by_ratio$percentage,
     main="Percentage of Commit Messages with Issues",
     ylab="Percentage",
     ylim=c(0,20),
-    names.arg=sorted_by_ratio$language,
+    names.arg=sorted_by_ratio$repository_language,
     col=heat.colors(nrow(tcir))
 )
 text(bp, 0, round(sorted_by_ratio$percentage, 1), cex=.8, pos=3)
